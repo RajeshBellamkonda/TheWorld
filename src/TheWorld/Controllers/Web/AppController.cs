0540e7a77a3542aceb6058fc1dcd0b2a51a4c7fa
Rajesh Bellamkonda <rajeshbala.b@gmail.com>
2017-02-16 11:48:51 +0000

    Logic & View Seperation Layout pages Configs & DI

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel contactVm)
+        {
+            if (contactVm.Email.Contains("test.com"))
+                ModelState.AddModelError("Email", "It's a fake address dude");
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], contactVm.Email, "From TheWorld", contactVm.Message);
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
